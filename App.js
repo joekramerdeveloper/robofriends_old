838857d2c7225cfadda372d6858cd46b13b8725f
Joe Kramer <95720434+joekramerdeveloper@users.noreply.github.com>
2022-01-06 12:51:21 -0600

    Added scrolling feature and some CSS upgrades

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React, { Component} from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
+// import { robots } from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 
 class App extends Component {
   constructor () {
     super ()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
+
+  }
+
+  componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(users => {this.setState({robots: users })})
   }
 
   onSearchChange = (event) => {
@@ -20,13 +28,19 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robots =>{
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
+    if (this.state.robots.length === 0) {
+      return <h1> loading... </h1>
+    } else {
   return(
     <div className='tc'>
       <h1> Robofriends </h1>
       <SearchBox searchChange={this.onSearchChange} /> 
+      <Scroll>
       <CardList robots={filteredRobots}/>
+      </Scroll>
     </div>
-  );
+    
+  );}
 }
 }
 
--- Scroll.js
+++ Scroll.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Scroll = (props) => {
-  return (
-    <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
-      {props.children}
-    </div>
-  );
+    return (
+        <div style={{overflow: 'scroll' , border: '2px solid #088D89' , height: '800px'}}>
+            {props.children}
+        </div>
+    );
 };
 
 export default Scroll;
